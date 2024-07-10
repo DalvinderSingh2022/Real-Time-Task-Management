cd18ef48194a5ecb8010cab59ed74b1b4a311083
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-07-10 23:42:50 +0530

    Loading Message now changes

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = ({ message }) => {
     return (
         <div className='full_container flex col loader'>
-            <div>{'Fetching Users details, please wait...'}</div>
+            <div>{message}</div>
             <span></span>
         </div>
     )
