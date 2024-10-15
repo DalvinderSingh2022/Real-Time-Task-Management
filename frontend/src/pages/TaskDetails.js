0580291cb8018fb4e0de0dcdf63887bb537b2c79
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-10-15 17:06:01 +0530

    invalid task id in url handled

--- frontend/src/pages/TaskDetails.js
+++ frontend/src/pages/TaskDetails.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
-import ViewTasks from '../components/ViewTasks';
-import TaksComments from '../components/TaksComments';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import styles from '../styles/taskdetails.module.css';
 
+import { TasksContext } from '../store/TasksContext';
+import NotFound from '../pages/NotFound';
+import ViewTasks from '../components/ViewTasks';
+import TaksComments from '../components/TaksComments';
+
 const TaskDetails = () => {
+    const { tasksState } = useContext(TasksContext);
+    const [task, setTask] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        setTask(tasksState.tasks.filter(task => task._id === id));
+    }, [tasksState, id]);
+
     return (
-        <div className={`${styles.container}`}>
-            <ViewTasks />
-            <TaksComments />
-        </div>
+        task?.length > 0
+            ? <div className={`${styles.container}`}>
+                <ViewTasks />
+                <TaksComments />
+            </div>
+            : task ? <NotFound /> : <div className="loading"></div>
     )
 }
 
