440dfa6505779361c061c1f2b6ee2e64c1efcf43
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-10-23 12:14:31 +0530

    Delete Button component created as common for all notification types

---
+++ frontend/src/components/Notifications/DeleteButton.js
@@ -0,0 +1,53 @@
+import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
+
+import { IoCloseSharp } from "react-icons/io5";
+
+import { NotificationsContext } from '../../store/NotificationContext';
+import { AppContext } from '../../store/AppContext';
+import Response from '../Response';
+
+const DeleteButton = ({ response, setResponse, prop }) => {
+    const { notificationsState, readNotification, deleteNotification } = useContext(NotificationsContext);
+    const { addToast } = useContext(AppContext);
+
+    useEffect(() => {
+        const notification = notificationsState.notifications.find(notification => notification._id === prop._id);
+
+        const handleNotification = () => {
+            if (notification && !notification.read) {
+                axios.put(`https://task-manager-v4zl.onrender.com/api/notifications/${prop._id}`, { ...notification, read: true })
+                    .then(({ data }) => {
+                        readNotification(data.updatedNotification._id);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            }
+        }
+
+        return () => handleNotification();
+    }, [notificationsState, readNotification, prop]);
+
+    const handleDelete = () => {
+        setResponse(true);
+        axios.delete(`https://task-manager-v4zl.onrender.com/api/notifications/${prop._id}`)
+            .then(() => {
+                deleteNotification(prop._id);
+            })
+            .catch((error) => {
+                addToast({ type: 'error', message: error?.response?.data?.message });
+                console.error(error);
+            })
+            .finally(() => setResponse(false));
+    }
+
+    return (
+        <>
+            {response && <Response />}
+            <button className='button round flex text_primary' title='Delete' onClick={handleDelete}><IoCloseSharp /></button>
+        </>
+    )
+}
+
+export default DeleteButton;
--- frontend/src/components/Notifications/TaskAssign.js
+++ frontend/src/components/Notifications/TaskAssign.js
@@ -1,67 +1,29 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { memo, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import { IoCloseSharp } from "react-icons/io5";
 import { MdNoteAdd } from "react-icons/md";
 
 import styles from "../../styles/notifications.module.css";
-import axios from 'axios';
+import DeleteButton from './DeleteButton';
 
-import { NotificationsContext } from '../../store/NotificationContext';
-import { AppContext } from '../../store/AppContext';
-import Response from '../Response';
-
-const TaskAssign = (notification) => {
+const TaskAssign = (prop) => {
     const [response, setResponse] = useState(false);
-    const { notificationsState, readNotification, deleteNotification } = useContext(NotificationsContext);
-    const { addToast } = useContext(AppContext);
-    const { title, description, dueDate } = notification.data.task;
-
-    useEffect(() => {
-        const notifi = notificationsState.notifications.find(notifi => notifi._id === notification._id);
-
-        if (notifi && !notifi.read) {
-            axios.put(`https://task-manager-v4zl.onrender.com/api/notifications/${notification._id}`, { ...notification, read: true })
-                .then(({ data }) => {
-                    readNotification(data.updatedNotification._id);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }
-
-    }, [notificationsState, readNotification, notification]);
-
-    const handleDelete = () => {
-        setResponse(true);
-        axios.delete(`https://task-manager-v4zl.onrender.com/api/notifications/${notification._id}`)
-            .then(() => {
-                deleteNotification(notification._id);
-            })
-            .catch((error) => {
-                addToast({ type: 'error', message: error?.response?.data?.message });
-                console.error(error);
-            })
-            .finally(() => setResponse(''));
-    }
+    const { title, createdAt, dueDate, _id } = prop.data.task;
 
     return (
         <>
-            {response && <Response />}
             <div className={`${styles.icon} button round flex`}><MdNoteAdd /></div>
             <div className="w_full">
-                <Link to={`/tasks/${'smsm'}`} className={`text_primary ${styles.message}`}>{notification.message}</Link>
+                <Link to={`/tasks/${_id}`} className={`text_primary ${styles.message}`}>{prop.message}</Link>
                 <div className={styles.data}>
-                    <div className='text_secondary'>Title: {title}</div>
-                    <div className='text_secondary'>Description: {description}</div>
-                    <div className='text_secondary'>Duedate: {new Date(dueDate).toDateString()}</div>
+                    <div className='text_primary'>Title: {title}</div>
+                    <div className='text_primary'>Due Date: {new Date(dueDate).toDateString()}</div>
+                    <div className='text_secondary'>{(new Date(createdAt).toDateString()) + " at " + (new Date(createdAt).toLocaleTimeString())}</div>
                 </div>
             </div>
-            {response
-                ? <div className='loading'></div>
-                : <button className='button round flex text_primary' title='Delete' onClick={handleDelete}><IoCloseSharp /></button>}
+            {response ? <div className='loading'></div> : <DeleteButton response={response} setResponse={setResponse} prop={prop} />}
         </>
     )
 }
 
-export default TaskAssign;
+export default memo(TaskAssign);
