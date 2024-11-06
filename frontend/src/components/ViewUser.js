6ab6042bef3ccba7284a0292905e431058310b6b
DalvinderSingh2022 <dalvinderSingh201999@gmail.com>
2024-11-06 19:23:48 +0530

    ViewUser Component added

---
+++ frontend/src/components/ViewUser.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+
+import styles from '../styles/userdetails.module.css';
+
+const ViewUser = (prop) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        if (!user) setUser(prop.user);
+    }, [prop, user]);
+
+    return (
+        <section className={`flex col ${styles.wrapper}`} onClick={e => e.stopPropagation()}>
+            {user
+                ? <form className='flex col'>
+                    <label htmlFor='profileImage'>
+                        <img
+                            src={user.profileImage || 'https://placehold.co/96x96'}
+                            alt={user.name}
+                        />
+                        <input
+                            type="file"
+                            accept="image/*"
+                            id="profileImage"
+                            disabled={true}
+                        />
+                    </label>
+                    <div>
+                        <input
+                            type="text"
+                            name='name'
+                            className='text_primary'
+                            value={user.name}
+                            disabled={true}
+                        />
+                        <div className="text_secondary">{user.email}</div>
+                        <div className="text-gray-600">Joined {new Date(user.createdAt).toDateString()}</div>
+                        <div className="text-gray-600 mt-2">Followers: {user.followers.length}</div>
+                    </div>
+                </form>
+                : <div className='loading'></div>}
+        </section>
+    )
+}
+
+export default ViewUser;
